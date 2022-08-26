b3a8c315254922efadb0f8c3139e1821da2f4da7
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-08-26 20:53:30 +0600

    new_youtube

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Services from "./pages/Services";
+
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
+
 function App() {
   return (
     <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/services" element={<Services />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
+      hg
     </div>
   );
 }
