7591e9a4350fbe572e69f245775d74019b277487
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-08-26 22:11:18 +0600

    search

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -37,8 +37,8 @@ export const categories = [
 export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
 export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
 export const demoVideoUrl = "/video/GDa8kZLNhJ4";
-export const demoChannelTitle = "JavaScript Mastery";
+export const demoChannelTitle = "Mahesh Biswas";
 export const demoVideoTitle =
-  "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI";
+  "YouTube Clone By Mahesh Biswas";
 export const demoProfilePicture =
   "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
