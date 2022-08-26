182baa8a26515a14f0abf5b018cfc3f9c414de1e
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-08-26 22:01:27 +0600

    logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
 import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  ChannelDetail,
+  Navbar,
+  Feed,
+  VideoDetail,
+  SearchFeed,
+} from "./components";
 
 function App() {
   return (
-    <div>
-      hg
-    </div>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
   );
 }
 
