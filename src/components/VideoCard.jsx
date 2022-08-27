6de024a5c04f4cf335baa518385bacda32d168c1
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-08-27 11:53:08 +0600

    icons

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,7 @@
-import { Card, CardMedia } from "@mui/material";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import {
   demoThumbnailUrl,
   demoChannelUrl,
@@ -32,6 +33,27 @@ const VideoCard = ({
           sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
         />
       </Link>
+      <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircleIcon
+              sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+            />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
